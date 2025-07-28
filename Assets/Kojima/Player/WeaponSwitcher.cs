274ba138dd64c9b08a229c99a49a1e0d7fc7a7b8
Uber020 <renanhs1@hotmail.com>
2025-07-28 20:20:07 -0300

    Update WeaponSwitcher.cs
    Throwing weapons is still broken

--- Assets/Kojima/Player/WeaponSwitcher.cs
+++ Assets/Kojima/Player/WeaponSwitcher.cs
@@ -1,62 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-/// <summary>
-///     Start()
-///         desliga o elemento de hud do timer
-///         carrega as armas do player
-///         atualiza as informacoes no HUD
-/// 
-///     Update()
-///         verifica inputs do usuario
-///
-///     LoadWeapon()
-///         realiza a logica pra pegar uma arma nova
-///
-///     SaveWeapon()
-///         salva a arma nova no inventario e aplica o offset na tela
-///
-///     SwitchWeapon()
-///         troca qual arma esta ativa na tela
-///
-///     ThrowWeapon()
-///         descarta a arma atual, caso o jogador aperte T
-///         ou pegue uma arma nova com inventario cheio
-/// 
-/// </summary>
 
 public class WeaponSwitcher : MonoBehaviour
 {
-    [Header("Variables")]
-    [SerializeField] private int selectedWeapon = 1;
-    [SerializeField] private int currentWeapon = 1;
-                     private bool isSwitching;
-                     private const int inventorySize = 3;
-                     private readonly Vector3 weaponOffset = new(0.35f, -0.4f, 0.5f);  // offset visual pra arma
-
-    [Header("References")] 
-    [SerializeField] private Camera mainCamera;
-                     private Transform inventory;
+    private int selectedWeapon;
+    private int currentWeapon;
+    private bool isSwitching;
+    private const int inventorySize = 3;
     
-    [Header("Declarations")]
+    private readonly Vector3 weaponOffset = new(0.35f, -0.4f, 0.5f);  // offset visual pra arma
+    private readonly List<GameObject> inventory = new();
+    
     private PlayerHUD playerHUD;
-    private WeaponController activeWeaponWC;
+    private WeaponController weaponController;
     
     
     private void Start()
     {
         playerHUD = GetComponent<PlayerHUD>();
-        inventory = mainCamera.transform;
-        
-        SaveWeapon(1);
-        SwitchWeapon();
-        
-        int i = 2;
-        while (transform.childCount > 2)
+
+        while (transform.childCount > 2)  // filhos do player: camera, hud e armas
         {
-            SaveWeapon(i);
-            inventory.GetChild(i).gameObject.SetActive(false);
-            ++i;
+            AddToInventory(transform.GetChild(2).gameObject);
+            inventory[^1].SetActive(false);
         }
+        
+        SwitchWeapon();  // seleciona a arma primaria
     }
     
     
@@ -64,32 +35,23 @@ public class WeaponSwitcher : MonoBehaviour
     {
         if (isSwitching) return;
         
-        if (Input.GetKeyDown(KeyCode.Equals)) LoadWeapon(); //{}{}
-        if (Input.GetKeyDown(KeyCode.T) && inventory.childCount > 2)
-        {
-            if (currentWeapon == 1)
-            {
-                selectedWeapon = Random.Range(2, inventory.childCount);
-            }
-            else
-            {
-                ThrowWeapon();
-            }
-        }
+        if (Input.GetKeyDown(KeyCode.Equals)) AddWeapon(transform.GetChild(2).gameObject);
+        if (Input.GetKeyDown(KeyCode.T)) ThrowCurrentWeapon();
         
         //se apertou pra trocar de arma
-        if      (Input.GetKeyDown(KeyCode.Alpha1)) selectedWeapon = 1;
-        else if (Input.GetKeyDown(KeyCode.Alpha2) && inventory.childCount >= 2) selectedWeapon = 2;
-        else if (Input.GetKeyDown(KeyCode.Alpha3) && inventory.childCount >= 3) selectedWeapon = 3;
+        if      (Input.GetKeyDown(KeyCode.Alpha1)) selectedWeapon = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && inventory.Count >= 2) selectedWeapon = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && inventory.Count >= 3) selectedWeapon = 2;
+        
         else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             --selectedWeapon;
-            if (selectedWeapon < 1) { selectedWeapon = inventory.childCount - 1; }
+            if (selectedWeapon < 1) { selectedWeapon = inventory.Count - 1; }
         }
         else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
             ++selectedWeapon;
-            if (selectedWeapon > inventory.childCount- 1) { selectedWeapon = 1; }
+            if (selectedWeapon > inventory.Count- 1) { selectedWeapon = 1; }
         }
         // inventorySize < selectedWeapon < 1
         //se da pra trocar de arma
@@ -97,95 +59,86 @@ public class WeaponSwitcher : MonoBehaviour
     }
 
     
-    private void LoadWeapon()
-    {
-        if (inventory.childCount <= inventorySize)  // inventario com espaco
-        {
-            SaveWeapon(inventory.childCount);
-            selectedWeapon = currentWeapon + 1;
-        }
-        else if (currentWeapon == 1)
-        {
-            currentWeapon = 2;
-        }
-        else
-        {
-            ThrowWeapon();
-            SaveWeapon(currentWeapon);
-            SwitchWeapon();
-        }
-    }
-
-    private void SaveWeapon(int index)
-    {
-        Transform newWeapon = transform.GetChild(2); //pega a arma nova
-        newWeapon.GetComponent<Rigidbody>().isKinematic = true;
-
-        //atualiza a posicao da arma comparando com o transform da camera
-        Vector3 weaponPosition = mainCamera.transform.position + weaponOffset;
-        
-        // testar isso se nao estiver considerando a rotacao em coordenadas locais da camera
-        // Vector3 weaponPosition = mainCamera.transform.position +
-                                 // mainCamera.transform.right * weaponOffset.x +
-                                 // mainCamera.transform.up * weaponOffset.y +
-                                 // mainCamera.transform.forward * weaponOffset.z;
-        
-        newWeapon.transform.position = weaponPosition;
-        // quaternion euler pra arrumar a rotacao do prefab de teste
-        newWeapon.transform.rotation = mainCamera.transform.rotation * Quaternion.Euler(90f, 0f, 0f); 
-        
-        newWeapon.SetParent(inventory);
-        newWeapon.SetSiblingIndex(index);
-    }
-
-    // private void PickWeapon()
-    // {
-    //     //remove rb
-    //     LoadWeapon();
-    // }
-
     private void SwitchWeapon()
     {
         isSwitching = true;
-        activeWeaponWC = GetComponentInChildren<WeaponController>();
-        inventory.GetChild(currentWeapon).gameObject.SetActive(false);
-        currentWeapon = selectedWeapon;
-        StartCoroutine(playerHUD.Timer(activeWeaponWC.data.switchTime, 
-            activeWeaponWC.data.switchTime, SwitchWeaponFinished));
+        inventory[currentWeapon].SetActive(false);
+        
+        // pega informacoes da arma selecionada
+        weaponController = inventory[selectedWeapon].GetComponent<WeaponController>();
+        
+        // pega informacoes da arma atual, caso nao de pra pegar a info da arma nova
+        // weaponController = GetComponentInChildren<WeaponController>();
+        
+        // usa a funcao de timer do HUD
+        StartCoroutine(playerHUD.Timer(weaponController.data.switchTime, 
+            weaponController.data.switchTime, EquipWeapon));
     }
     
-    private void SwitchWeaponFinished()
+    
+    private void EquipWeapon()
     {
-        inventory.GetChild(selectedWeapon).gameObject.SetActive(true);
-        activeWeaponWC = GetComponentInChildren<WeaponController>();
-        activeWeaponWC.enabled = true;
-        activeWeaponWC.UpdatePlayerHUD();
+        currentWeapon = selectedWeapon;
+        inventory[currentWeapon].SetActive(true);
+        weaponController = inventory[currentWeapon].GetComponent<WeaponController>();
+        weaponController.UpdatePlayerHUD();
+        
         isSwitching = false;
     }
     
-    private void ThrowWeapon()
+    
+    private void AddWeapon(GameObject weapon)
     {
-        isSwitching = true;
-        Transform thrownWeapon = inventory.GetChild(currentWeapon);
-        if (activeWeaponWC.timerC !=  null) { StopCoroutine(activeWeaponWC.timerC); }
-        thrownWeapon.SetParent(null);
-        activeWeaponWC.enabled = false;
+        if (inventory.Count < inventorySize) { AddToInventory(weapon); }
+        else if (currentWeapon == 0) { selectedWeapon = Random.Range(1, inventory.Count); }  // cant throw primary weapon
+        else  // but can throw other weapons
+        {
+            ThrowCurrentWeapon();
+            AddToInventory(weapon);
+        }
         
-        Rigidbody thrownWeaponRB = thrownWeapon.GetComponent<Rigidbody>();
-        thrownWeaponRB.isKinematic = false;
-        thrownWeaponRB.AddForce(mainCamera.transform.forward * 2f);
-        
-        selectedWeapon = inventory.childCount - 1;
-        currentWeapon = selectedWeapon;
-        StartCoroutine(playerHUD.Timer(activeWeaponWC.data.switchTime, 
-            activeWeaponWC.data.switchTime, SwitchWeaponFinished));
+        SwitchWeapon();
     }
+    
+    
+    private void AddToInventory(GameObject weapon)
+    {
+        inventory.Add(weapon);
+        ApplyWeaponOffset(weapon);
+        selectedWeapon = inventory.Count - 1;
+    }
+    
+    
+    private void ApplyWeaponOffset(GameObject weapon)
+    {
+        // coloca a arma como filho da camera e aplica um offset
+        Transform cameraT = transform.GetChild(0);
+        weapon.transform.SetParent(cameraT);
+        weapon.transform.position = cameraT.position + weaponOffset;
+        // quaternion euler pra arrumar a rotacao dos prefabs de teste, tem que mudar depois
+        weapon.transform.rotation = cameraT.rotation * Quaternion.Euler(90f, 0f, 0f); 
+    }
+    
+    
+    private void ThrowCurrentWeapon()
+    {
+        if (currentWeapon == 1)
+        {
+            selectedWeapon = Random.Range(1,  inventory.Count);
+        }  // cant throw primary weapon
+        else
+        {
+            // FAZER ISSO NAO DESLIGA AS FUNCOES DE ATIRAR
+            inventory[currentWeapon].transform.SetParent(null);  // parent to the game tree
+            inventory.RemoveAt(currentWeapon);
+            selectedWeapon = inventory.Count - 1;
+        }
+
+        SwitchWeapon();
+    }
+    
     
     // kinematic = true: voce quem mexe o objeto pelo script ou parents, o sistema de fisica da unity nao interage
     // kinematic = false: a fisica da unity interage com o objeto
-
-    // public void Stop()
-    // {
-    //     if (isSwitching) StopCoroutine(switchingC);
-    // }
+    
 }
